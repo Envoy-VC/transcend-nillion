762e4244d1c92b94b91af8c190d364c0bc57b575
Vedant Chainani <vedantchainani1084@gmail.com>
2024-05-29 21:14:33 +0530

    chore: add simple IPFS Relay server

---
+++ packages/relay-server/.eslintrc.cjs
@@ -0,0 +1,71 @@
+const { resolve } = require('node:path');
+
+// @ts-expect-error no types
+const { JAVASCRIPT_FILES } = require('@vercel/style-guide/eslint/constants');
+
+const project = resolve(__dirname, 'tsconfig.json');
+
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    require.resolve('@vercel/style-guide/eslint/browser'),
+    require.resolve('@vercel/style-guide/eslint/node'),
+    require.resolve('@vercel/style-guide/eslint/react'),
+    require.resolve('@vercel/style-guide/eslint/next'),
+    require.resolve('@vercel/style-guide/eslint/typescript'),
+  ],
+  parserOptions: { project },
+  settings: {
+    'import/resolver': { typescript: { project } },
+  },
+  exclude: ['.eslint.cjs'],
+  rules: {
+    'no-console': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-confusing-void-expression': [
+      'error',
+      { ignoreArrowShorthand: true },
+    ],
+    '@typescript-eslint/no-shadow': 'off',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      { checksVoidReturn: { attributes: false } },
+    ],
+    '@typescript-eslint/restrict-template-expressions': ['warn'],
+    'react/function-component-definition': [
+      'warn',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/jsx-sort-props': [
+      'warn',
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        multiline: 'last',
+        reservedFirst: true,
+      },
+    ],
+    'import/order': [
+      'off',
+      {
+        'newlines-between': 'ignore',
+        alphabetize: { order: 'asc' },
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: JAVASCRIPT_FILES,
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+    },
+    {
+      files: ['**/*.d.ts'],
+      rules: { 'import/no-default-export': 'off' },
+    },
+  ],
+};
